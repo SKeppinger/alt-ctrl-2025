a25cfd3d54c87bb3abb7c1f18564a4c89ca9e4a8
Matthew Saenz <matthewjamessaenz@gmail.com>
2025-11-16 16:07:20 -0500

    removed unused analog inputs

--- game/drill_controller_states.gd
+++ game/drill_controller_states.gd
@@ -5,5 +5,3 @@ var switch_state : bool
 
 var force_sensor_0_value : int
 var force_sensor_1_value : int
-var force_sensor_2_value : int
-var force_sensor_3_value : int
--- game/read_serial_input.gd
+++ game/read_serial_input.gd
@@ -26,13 +26,11 @@ func _process(delta: float) -> void:
 		# packet is well formed
 		input_state_str = input_state_str.substr(1, input_state_str.length()-2)
 		var input_state_arr = input_state_str.split(',')
-		if (input_state_arr.size() == 6):
+		if (input_state_arr.size() == 4):
 			var button_state = bool(int(input_state_arr[0])) # True if button is pressed, false otherwise
 			var switch_state = bool(int(input_state_arr[1])) # True if switch is closed, false otherwise
 			var force_sensor_0_value = int(input_state_arr[2])
 			var force_sensor_1_value = int(input_state_arr[3])
-			var force_sensor_2_value = int(input_state_arr[4])
-			var force_sensor_3_value = int(input_state_arr[5])
 			
 			if not drill_controller_states.button_state and button_state:
 				drill_press.emit()
@@ -41,12 +39,8 @@ func _process(delta: float) -> void:
 			drill_controller_states.switch_state = switch_state
 			drill_controller_states.force_sensor_0_value = force_sensor_0_value
 			drill_controller_states.force_sensor_1_value = force_sensor_1_value
-			drill_controller_states.force_sensor_2_value = force_sensor_2_value
-			drill_controller_states.force_sensor_3_value = force_sensor_3_value
 			
 	print(drill_controller_states.button_state, ',', 
 		  drill_controller_states.switch_state, ',', 
 		  drill_controller_states.force_sensor_0_value, ',', 
-		  drill_controller_states.force_sensor_1_value, ',', 
-		  drill_controller_states.force_sensor_2_value, ',', 
-		  drill_controller_states.force_sensor_3_value)
+		  drill_controller_states.force_sensor_1_value)
